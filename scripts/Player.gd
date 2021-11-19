0b4f17e7ff6db595b85aea2c923672fdc2cc73ec
MrRedaM <r.morsli@esi-sba.dz>
2021-11-19 17:38:34 +0100

    Initial commit

---
+++ scripts/Player.gd
@@ -0,0 +1,16 @@
+extends KinematicBody2D
+
+const UP_DIRECTION := Vector2.UP
+
+export var speed := 600.0
+export var jump_strength := 1500.0
+export var max_jumps := 2
+export var double_jump_strength := 1200.0
+export var gravity := 4500.0
+
+func _ready():
+	pass
+
+
+func _process(delta):
+	pass
